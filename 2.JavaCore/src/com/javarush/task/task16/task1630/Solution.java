b9e3f0efdb22c529190dac3c2951230647e5c192
Vladimir Belkin <vladimirB2000@mail.ru>
2020-12-25 17:42:30 +0700

    increment synchronized

--- 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1630/Solution.java
@@ -4,8 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
-/* 
+/*
 Последовательный вывод файлов
 */
 
@@ -13,24 +15,29 @@ public class Solution {
     public static String firstFileName;
     public static String secondFileName;
 
-    //add your code here - добавьте код тут
+    static {
+        Scanner sc1 = new Scanner(System.in);
+        firstFileName = sc1.nextLine();
+        secondFileName = sc1.nextLine();
+        sc1.close();
+    }//add your code here - добавьте код тут
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
         systemOutPrintln(firstFileName);
         systemOutPrintln(secondFileName);
     }
 
-    public static void systemOutPrintln(String fileName) throws InterruptedException {
+    public static void systemOutPrintln(String fileName) throws InterruptedException, FileNotFoundException {
         ReadFileInterface f = new ReadFileThread();
         f.setFileName(fileName);
         f.start();
-        //add your code here - добавьте код тут
+        f.join();//add your code here - добавьте код тут
         System.out.println(f.getFileContent());
     }
 
     public interface ReadFileInterface {
 
-        void setFileName(String fullFileName);
+        void setFileName(String fullFileName) throws FileNotFoundException;
 
         String getFileContent();
 
@@ -39,5 +46,34 @@ public class Solution {
         void start();
     }
 
-    //add your code here - добавьте код тут
+    public static class ReadFileThread extends Thread implements ReadFileInterface{
+        private String st = "";
+        private String fullFileName;
+        @Override
+        public void setFileName(String fullFileName)  {
+            this.fullFileName = fullFileName;
+
+        }
+
+        @Override
+        public String getFileContent() {
+
+            return st;
+        }
+
+        public void run(){
+            try {
+                Scanner sc2 = new Scanner(new File(fullFileName));
+                while (sc2.hasNext()){
+                    String st2 = sc2.nextLine();
+                    st = st + st2;
+
+                }
+                sc2.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("No file found");;
+            }
+
+        }
+    }//add your code here - добавьте код тут
 }
