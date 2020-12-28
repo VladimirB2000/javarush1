e1792046e442ed7d1b2ccf2d76d6cde73ff81e76
Vladimir Belkin <vladimirB2000@mail.ru>
2020-12-28 10:49:55 +0700

    frequent byte count

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task18.task1803;
+
+import java.io.*;
+import java.util.*;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        FileInputStream fileInputStream = new FileInputStream(sc.nextLine());
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> treemap = new TreeMap<>();
+        while ((fileInputStream.available()> 0 )){
+            int data = fileInputStream.read();
+            list.add(data);
+        }
+        for (Integer temp: list){
+            Integer count = treemap.get(temp);
+            treemap.put(temp, (count == null) ? 1 : count +1);
+        }
+        ArrayList<Integer> values = new ArrayList<>(treemap.values());
+        Collections.sort(values);
+        Collection<Integer> collection =treemap.keySet();
+        for (Integer key: collection){
+            Integer n = treemap.get(key);
+            if(key != null){
+                if(values.get(values.size()-1).equals(n)){
+                    System.out.print(key + " ");
+                }
+            }
+        }
+        sc.close();
+        fileInputStream.close();
+    }
+}
