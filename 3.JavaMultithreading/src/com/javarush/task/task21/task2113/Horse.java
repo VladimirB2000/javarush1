76b4b8ad1bf16244e02ae11485cebf0b1c9c3ffc
Vladimir Belkin <vladimirB2000@mail.ru>
2021-03-13 16:50:20 +0700

    Hippodrome 14

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -40,6 +40,9 @@ public class Horse {
     }
 
     public void print(){
-
+        for (int i= 0; i< (int) distance; i++){
+            System.out.print(".");
+        }
+        System.out.println(name);
     }
 }
