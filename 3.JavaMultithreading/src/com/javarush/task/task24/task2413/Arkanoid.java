67b21c6b6d07bae20ffc01d7fa5fe78ecb7c6974
Vladimir Belkin <vladimirB2000@mail.ru>
2021-03-25 14:06:16 +0700

    Arkanoid 2

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,7 +1,28 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+
+    private  int width;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    private  int height;
     public static void main(String[] args) {
 
     }
 }
+
