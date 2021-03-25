343c64e37998f25027fab9c3e41bb02b0777c084
Vladimir Belkin <vladimirB2000@mail.ru>
2021-03-25 15:43:31 +0700

    Arkanoid 10

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -34,4 +34,17 @@ public class Canvas {
         this.height = height;
         matrix = new char[height+2][width+2];
     }
+    void setPoint(double x, double y, char c){
+        if(x>0 && y>0 && y< matrix.length && x< matrix.length){
+            matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+        }
+    }
+
+    void drawMatrix(double x, double y, int[][] matrix, char c){
+        for (int i = 0; i< matrix.length; i++){
+            for (int j =0; j< matrix[i].length; j++){
+                if(matrix[i][j] !=0) setPoint(x+j, y+i, c);
+            }
+        }
+    }
 }
