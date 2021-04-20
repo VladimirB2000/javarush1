f1c1b246f63ea4dbe031218885a09bf9c56ab5ab
Vladimir Belkin <vladimirB2000@mail.ru>
2021-04-20 10:28:25 +0700

    Chat 2

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,32 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    static String readString(){
+        while (true){
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+    static int readInt(){
+        while (true){
+            try {
+                return Integer.parseInt(readString());
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+
+    }
 }
