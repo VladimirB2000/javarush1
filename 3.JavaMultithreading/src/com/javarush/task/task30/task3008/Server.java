021bb62289aaa715672c2f7e8847993fe05a9806
Vladimir Belkin <vladimirB2000@mail.ru>
2021-04-22 11:14:58 +0700

    Chat 6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,31 @@
 package com.javarush.task.task30.task3008;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
     public static void main(String[] args) {
+        int port = ConsoleHelper.readInt();
+        try(ServerSocket ss = new ServerSocket(port)){
+
+            ConsoleHelper.writeMessage("Server started");
+            while (true){
+                Handler handler = new Handler(ss.accept());
+                handler.start();
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
+
