d36fa1418095ecf1d9014d0f50eb661354fab9f8
Vladimir Belkin <vladimirB2000@mail.ru>
2021-04-24 17:22:32 +0700

    chat 18

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task30.task3008.client;
+
+public class BotClient extends Client{
+    public class BotSocketThread extends SocketThread {
+    }
+        protected SocketThread getSocketThread() {
+            return new BotSocketThread();
+        }
+        protected boolean shouldSendTextFromConsole(){
+            return false;
+        }
+        protected String getUserName(){
+            return "date_bot_"+(int) (Math.random()*100);
+        }
+
+
+    public static void main(String[] args) {
+        BotClient botClient = new BotClient();
+        botClient.run();
+    }
+}
